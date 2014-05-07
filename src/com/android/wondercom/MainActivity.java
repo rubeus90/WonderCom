181ac300fcff91d67dcbffe955a051b4176473cd
rubeus <rubeus90@gmail.com>
2014-05-07 09:40:45 +0800

    Copy wpa_supplicant file to external storage then to /system/bin
    Backup the original file to private external storage

--- src/com/android/wondercom/MainActivity.java
+++ src/com/android/wondercom/MainActivity.java
@@ -1,29 +1,33 @@
 package com.android.wondercom;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
+import android.content.res.AssetManager;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
 public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); 
+        
+        createExternalStoragePrivateFile();
+        copyWifiConfigFile();
+        reboot();
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
@@ -38,5 +42,51 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+    
+    public void gainSuperuserRight(){
+    	try {
+			Runtime.getRuntime().exec("su");			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+    
+    public void createExternalStoragePrivateFile() {
+        File file = new File(getExternalFilesDir(null), "wpa_supplicant");
+
+        try {
+        	AssetManager assetManager = getAssets();
+        	InputStream input = assetManager.open("wpa_supplicant");
+            OutputStream output = new FileOutputStream(file);
+            byte[] data = new byte[input.available()];
+            input.read(data);
+            output.write(data);
+            input.close();
+            output.close();
+        } catch (IOException e) {
+            Log.w("ExternalStorage", "Error writing " + file, e);
+        }
+    }
+    
+    public void copyWifiConfigFile(){
+    	try {
+			Runtime.getRuntime().exec( "mount -o rw,remount -t yaffs2 /dev/block/mtdblock3 /system" );
+			Runtime.getRuntime().exec( "cp /system/bin/wpa_supplicant "+ getExternalFilesDir(null)+ "/wpa_supplicant.original" );
+			Runtime.getRuntime().exec( "cp "+ getExternalFilesDir(null) +"/wpa_supplicant /system/bin/." );
+			Runtime.getRuntime().exec( "chmod 755 /system/bin/wpa_supplicant" );
+			Runtime.getRuntime().exec( "chown system.wifi /data/misc/wifi/wpa_supplicant.conf" );
+			Runtime.getRuntime().exec( "exit" );			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+    
+    public void reboot(){
+    	try{
+    		Runtime.getRuntime().exec("reboot");
+    	} catch( IOException e){
+    		e.printStackTrace();
+    	}
     }
 }
