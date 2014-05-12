86b59365c5de821490eb02475e5921ccebc8c9ba
rubeus <rubeus90@gmail.com>
2014-05-12 13:58:41 +0800

    Create a new ad-hoc network using existing codes of AdhocConfigDemo app Works on Galaxy Nexus since ad-hoc network is already enabled in Cyanogenmod rom for this device

--- src/com/android/wondercom/MainActivity.java
+++ src/com/android/wondercom/MainActivity.java
@@ -1,52 +1,84 @@
 package com.android.wondercom;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.HashSet;
+import java.net.InetAddress;
 
-import org.span.manager.ManetManagerApp;
-import org.span.service.ManetObserver;
-import org.span.service.core.ManetService.AdhocStateEnum;
-import org.span.service.routing.Node;
-import org.span.service.system.ManetConfig;
-
-import android.content.SharedPreferences;
-import android.content.res.AssetManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.WifiConfiguration.KeyMgmt;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity implements ManetObserver {
-	public static final String TAG = "MainActivity";
-	
+public class MainActivity extends ActionBarActivity {
+	public static final String TAG = "MainActivity";	
 	public static final String CONFS= "confs";
-	private static ManetManagerApp app = null;
+	
+	private WifiManager mWifiManager;
+    private WifiManagerNew mWifiManagerNew;
+    private IntentFilter mFilter;
+    private BroadcastReceiver mReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); 
         
-//        gainSuperuserRight();
-//        if(isFirstLaunch()){
-//        	Log.i("First configuration", "First launch!");
-//            memorizeFirstLaunch();
-            app = (ManetManagerApp)getApplication();
-            app.manet.registerObserver(this);
-            
-            app.manet.sendStartAdhocCommand();
-//        }
-//        else{
-//        	Log.i("First configuration", "Not first launch!");
-//        }
+        mWifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
+
+        /* Wrap WifiManager to access new methods */
+        mWifiManagerNew = new WifiManagerNew(mWifiManager);
+
+        /* Register broadcast receiver to get notified when Wifi has been enabled */
+        mFilter = new IntentFilter();
+        mFilter.addAction(WifiManager.WIFI_STATE_CHANGED_ACTION);
+
+        mReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+
+                if (WifiManager.WIFI_STATE_CHANGED_ACTION.equals(action)) {
+                    int state = intent.getIntExtra(WifiManager.EXTRA_WIFI_STATE,
+                            WifiManager.WIFI_STATE_UNKNOWN);
+
+                    if (state == WifiManager.WIFI_STATE_ENABLED) {
+                        log("Wifi state enabled");
+
+                        /* This function only returns valid results in enabled
+                         * state and is not part of the standard API (yet?) */
+                        if (mWifiManagerNew.isIbssSupported()) {
+                            log("Ad-hoc mode is supported");
+
+                            configureAdhocNetwork();
+
+                            log("Successfully configured Adhoc network!");
+                        } else {
+                            log("Sorry, Ad-hoc mode is not supported by your system or device!");
+                        }
+                    }
+                }
+            }
+        };
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        registerReceiver(mReceiver, mFilter);
+        mWifiManager.setWifiEnabled(true);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterReceiver(mReceiver);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -56,9 +88,6 @@ public class MainActivity extends ActionBarActivity implements ManetObserver {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
@@ -66,123 +95,43 @@ public class MainActivity extends ActionBarActivity implements ManetObserver {
         return super.onOptionsItemSelected(item);
     }
     
-    public void gainSuperuserRight(){
-    	try {
-			Runtime.getRuntime().exec("su");			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    private void log(String msg) {
+        Log.d(TAG, msg);
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
     
-    public void createExternalStoragePrivateFile() {
-        File file = new File(getExternalFilesDir(null), "wpa_supplicant");
-
+    private void configureAdhocNetwork() {
         try {
-        	AssetManager assetManager = getAssets();
-        	InputStream input = assetManager.open("wpa_supplicant");
-            OutputStream output = new FileOutputStream(file);
-            byte[] data = new byte[input.available()];
-            input.read(data);
-            output.write(data);
-            input.close();
-            output.close();
-        } catch (IOException e) {
-            Log.w("ExternalStorage", "Error writing " + file, e);
+            /* We use WifiConfigurationNew which provides a way to access 
+             * the Ad-hoc mode and static IP configuration options which are 
+             * not part of the standard API yet */
+            WifiConfigurationNew wifiConfig = new WifiConfigurationNew();
+
+            /* Set the SSID and security as normal */
+            wifiConfig.SSID = "\"WonderCom\"";
+            wifiConfig.allowedKeyManagement.set(KeyMgmt.NONE);
+
+            /* Use reflection until API is official */
+            wifiConfig.setIsIBSS(true);
+            wifiConfig.setFrequency(2442);
+
+            /* Use reflection to configure static IP addresses */
+            wifiConfig.setIpAssignment("STATIC");
+            wifiConfig.setIpAddress(InetAddress.getByName("10.0.0.2"), 24);
+            wifiConfig.setGateway(InetAddress.getByName("10.0.0.1"));
+            wifiConfig.setDNS(InetAddress.getByName("8.8.8.8"));
+
+            /* Add, enable and save network as normal */
+            int id = mWifiManager.addNetwork(wifiConfig);
+            if (id < 0) {
+                log("Failed to add Ad-hoc network");
+            } else {
+                mWifiManager.enableNetwork(id, true);
+                mWifiManager.saveConfiguration();
+            }
+        } catch (Exception e) {
+            log("Wifi configuration failed!");
+            e.printStackTrace();
         }
     }
-    
-    public void copyWifiConfigFile(){    	
-    	File sdCard = Environment.getExternalStorageDirectory();
-    	File dir = new File (sdCard.getAbsolutePath() + "/WonderCom");
-    	dir.mkdirs();
-    	
-    	try {
-			Runtime.getRuntime().exec( "mount -o rw,remount -t yaffs2 /dev/block/mtdblock3 /system" );
-			Runtime.getRuntime().exec( "cp /system/bin/wpa_supplicant " + sdCard.getAbsolutePath() + "/WonderCom/wpa_supplicant.original" );
-			Runtime.getRuntime().exec( "cp /storage/sdcard0/wpa_supplicant /system/bin/" );
-			Runtime.getRuntime().exec( "chmod 755 /system/bin/wpa_supplicant" );
-			Runtime.getRuntime().exec( "chown system.wifi /data/misc/wifi/wpa_supplicant.conf" );		
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-    }
-    
-    public void reboot(){
-    	try{
-    		Runtime.getRuntime().exec(new String[]{"/system/bin/su","-c","reboot now"});
-    	} catch( IOException e){
-    		e.printStackTrace();
-    	}
-    }
-    
-    public void memorizeFirstLaunch(){
-    	SharedPreferences settings = getSharedPreferences(CONFS, 0);
-        SharedPreferences.Editor editor = settings.edit();
-        editor.putBoolean("firstLaunch", false);
-        editor.commit();
-    }
-    
-    public boolean isFirstLaunch(){
-    	SharedPreferences settings = getSharedPreferences(CONFS, 0);
-        return settings.getBoolean("firstLaunch", true);
-    }
-
-
-	@Override
-	public void onServiceConnected() {
-		Log.d(TAG, "onServiceConnected()"); 
- 		app.manet.sendManetConfigQuery();
- 		app.manet.sendAdhocStatusQuery();
-	}
-
-
-	@Override
-	public void onServiceDisconnected() {
- 		Log.d(TAG, "onServiceDisconnected()"); 
-	}
-
-
-	@Override
-	public void onServiceStarted() {
-		Log.d(TAG, "onServiceStarted()");
-	}
-
-
-	@Override
-	public void onServiceStopped() {
-		Log.d(TAG, "onServiceStopped()");
-	}
-
-
-	@Override
-	public void onAdhocStateUpdated(AdhocStateEnum state, String info) {
-		Log.d(TAG, "onAdhocStateUpdated()"); // DEBUG
-//		showAdhocMode(state);
-		app.displayToastMessage(info);
-	}
-
-
-	@Override
-	public void onConfigUpdated(ManetConfig manetcfg) {
-		Log.d(TAG, "onConfigUpdated()"); // DEBUG
-//		showRadioMode(manetcfg.isUsingBluetooth());
-//		displayIPandSSID(manetcfg);
-	}
-
-
-	@Override
-	public void onPeersUpdated(HashSet<Node> peers) {
-		Log.d(TAG, "onPeersUpdated()");
-	}
-
-
-	@Override
-	public void onRoutingInfoUpdated(String info) {
-	}
-
-
-	@Override
-	public void onError(String error) {
-		Log.d(TAG, "onError()");
-	}
 }
