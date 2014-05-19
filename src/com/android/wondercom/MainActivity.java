251f1210138a5f1d55000d0f1cd58871d1d646ed
rubeus90 <nghongngoc90@gmail.com>
2014-05-19 09:30:22 -0700

    Application can make Wi-Fi P2P method calls and receive Wi-Fi P2P intents. The broadcast receiver and intent filter are set up

--- src/com/android/wondercom/MainActivity.java
+++ src/com/android/wondercom/MainActivity.java
@@ -1,5 +1,9 @@
 package com.android.wondercom;
 
+import android.content.Context;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.WifiP2pManager.Channel;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
@@ -9,21 +13,37 @@ import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 	public static final String TAG = "MainActivity";	
+	private WifiP2pManager mManager;
+	private Channel mChannel;
+	private WifiDirectBroadcastReceiver mReceiver;
+	private IntentFilter mIntentFilter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); 
+        
+        mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(this, getMainLooper(), null);
+        mReceiver = new WifiDirectBroadcastReceiver(mManager, mChannel, this);
+        
+        mIntentFilter = new IntentFilter();
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
     }
 
     @Override
     public void onResume() {
         super.onResume();
+        registerReceiver(mReceiver, mIntentFilter);
     }
 
     @Override
     public void onPause() {
         super.onPause();
+        unregisterReceiver(mReceiver);
     }
 
     @Override
